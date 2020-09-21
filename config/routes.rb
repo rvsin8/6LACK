153ab67310cc02e7f1e100bf1d80cf40f53b5a6c
“Ravneet <your_email_here>
2020-09-21 17:44:28 -0400

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+    ##namespace :api, defaults: {format: :json} do
+    ##  resources :messages, only: [:create, :update, :destroy]
+    ##  resources :channels, only: [:index, :create, :show, :update, :destory]
+    ##  resources :user, only: [:create, :index, :update, :destroy]
+    ##  resources :session, only: [:create, :destroy]
+    ##  resources :directmessages, only: [:index, :create, :update, :destroy]
+  end
+
+  root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- frontend/6lack.jsx
+++ frontend/6lack.jsx
@@ -5,3 +5,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to 6lack</h1>, root);
 });
+
+//*ask daniel why we do this
