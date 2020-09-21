ba3c54911ff9c05b0a47e0a199fa832a03dd9c32
“Ravneet <your_email_here>
2020-09-21 18:00:12 -0400

    anything

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     ##  resources :user, only: [:create, :index, :update, :destroy]
     ##  resources :session, only: [:create, :destroy]
     ##  resources :directmessages, only: [:index, :create, :update, :destroy]
-  end
+  
 
   root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
