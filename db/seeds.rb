c16378bbdcd031798bf134d0f2f495d4828dd1a4
“Ravneet <your_email_here>
2020-09-28 15:26:43 -0400

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,26 +16,3 @@ aubrey = User.create!(username: "drake", password: "popstar", email: "drizzy@ovo
 khalid = User.create!(username: "khalid", password: "location", email: "khalid@rca.com")
 sean = User.create!(username: "jayz", password: "blueprint", email: "hov@rocafella.com")
 
-grammy_committee = Channel.create!(admin_id: sixlack.id, name: "grammy-nominations", private: false)
-record_labels = Channel.create!(admin_id: sixlack.id, name: "new-signings", private: false)
-collaboration = Channel.create!(admin_id: sixlack.id, name: "teamwork", private: false)
-
-grammy-committee.users << sixlack
-grammy-committee.users << jermaine
-grammy-committee.users << future
-grammy-committee.users << aubrey
-
-record-labels.users << sean
-record-labels.users << sixlack
-record-labels.users << khalid
-record-labels.users << jermaine
-
-collaboration.users << khalid
-collaboration.users << future
-
-Message.create!(content:"2 grammy nominations!", 	channel_id: channel_1.id, sender_id: sixlack.id)
-Message.create!(content:"good job!", 	channel_id: channel_1.id, sender_id: jermaine.id)
-Message.create!(content:"glad I was apart of it", 	channel_id: channel_1.id, sender_id: jermaine.id)
-Message.create!(content:"congratulations!", 	channel_id: channel_1.id, sender_id: sean.id)
-Message.create!(content:"keep working hard!", 	channel_id: channel_1.id, sender_id: aubrey.id)
-Message.create!(content:"feature coming soon", 	channel_id: channel_1.id, sender_id: aubrey.id)
