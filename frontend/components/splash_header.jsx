3e138401912394d3613de150e02d7ab259711ffa
“Ravneet <your_email_here>
2020-09-29 13:51:23 -0400

    auth done

--- frontend/components/splash_header.jsx
+++ frontend/components/splash_header.jsx
@@ -20,77 +20,119 @@ class SplashPage extends React.Component {
             return this.props.history.push('/signup');
         });
     }
+    
+    handleClick() {
+        this.props.action({ username: 'ricardo', email: 'sixlack@interscope.com', password: 'prettylittlefears' });
+    }
+    
     render() {
         return (
-            <div className="splash-div">
-                <div> 
-                     <div className="firstcol"> 
-         
-                        <h1 className="splash-hq">
-                            6lack is where collaborations happens
-                        </h1> 
-
-                        <div className="team">
-                            With channels in 6lack, you and your team know where to go to ask questions, share updates and stay in the loop on your music.
-                        </div>
-                        <br/>
-                        <form className="splash-form" onSubmit={this.handleSubmit}>
-                            <input type="submit" className="splash-started" value="TRY FOR FREE" /> </form>
-                        <br/>
-
-                        <video className="video"
-                            autoPlay
-                            loop
-                            src="https://a.slack-edge.com/9b527/marketing/img/homepage/hp-prospect/hero/desktop/mp4/hero-image.mp4"
-                            type="video/mp4"
-                            id="animation1"
-                            muted/>
-        
-                        
-                    </div> 
-
-                    
-                </div>
-                <div className="header-div-2">
-                    <h2 className="better-way">
-                        A better way to share your art
-                    </h2>
-                        <p className="unlike-text">
-                            Unlike email, conversations in 6lack are easy to follow. And they’re more than conversations — you can make calls, share music files, and even connect with other musicians on apps.
-                        </p>
+            <div className="ultra-div">
+                <div className="splash-div">
                     <div>
-                        <video className="animation-1"
-                            autoPlay
-                            loop
-                            src="https://a.slack-edge.com/5f6f2/marketing/img/homepage/hp-prospect/channels/bring-your-team-together-in-channels.mp4"
-                            type="video/mp4"
-                            id="animation2"
-                            muted
-                        />
-                    </div>
-                        <form className="splash-form" onSubmit={this.handleSubmit}>
-                            <input type="submit" className="splash-started2" value="SEE ALL FEATURES" />
-                        </form>
-                </div>
-  
+                        <div className="firstcol">
+                            <div>
+                                <h1 className="splash-hq">
+                                    6lack is where collaborations happens
+                            </h1>
 
-                <div className="header-div-3">
-                    <h2 className="better-way-2">
-                        Over 750,000 artists use 6lack to get collaborations done
+                                <div className="team">
+                                    With channels in 6lack, you and your team know where to go to ask questions, share updates and stay in the loop on your music.
+                            </div>
+                                <br />
+                                <form className="splash-form" onSubmit={this.handleSubmit}>
+                                    <input type="submit" className="splash-started" value="TRY FOR FREE" />
+                                </form>
+                                <br />
+                            </div>
+
+
+
+
+                            <video className="video"
+                                autoPlay
+                                loop
+                                src="https://a.slack-edge.com/9b527/marketing/img/homepage/hp-prospect/hero/desktop/mp4/hero-image.mp4"
+                                type="video/mp4"
+                                id="animation1"
+                                muted
+                            />
+
+
+                        </div>
+
+
+                    </div>
+                    <div className="header-div-2">
+                        <div>
+                            <video className="animation-1"
+                                autoPlay
+                                loop
+                                src="https://a.slack-edge.com/5f6f2/marketing/img/homepage/hp-prospect/channels/bring-your-team-together-in-channels.mp4"
+                                type="video/mp4"
+                                id="animation2"
+                                muted
+                            />
+                        </div>
+
+                        <div>
+                            <div className="seccol">
+                                <h2 className="better-way">
+                                    A better way to share your art
+                                </h2>
+                                <p className="unlike-text">
+                                    Unlike email, conversations in 6lack are easy to follow. And they’re more than conversations — you can make calls, share music files, and even connect with other musicians on apps.
+                                </p>
+                                <form className="splash-form" onClick={() => this.handleClick()}>
+                                    <input type="submit" className="splash-started" value="Demo" />
+                                </form>
+
+
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+
+
+
+
+
+                    </div>
+
+
+                    <div className="header-div-3">
+                        <h2 className="better-way-2">
+                            Over 750,000 artists use 6lack to get collaborations done
                     </h2>
-                    <div className= "youtube">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/iSgUMPHQEWw"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <div>
+                            <div className="thirdcol">
+                                <p className="unlike-text-2">
+                                    “I help share my sound from the studio to others in my neighborhood, and all around the world” Valentine explains. “[With 6lack] there’s no wasted time or wasted music because you’re able to communicate things right away.”
+                                    Ricardo Valdez Valentine
+                                    Artist, East Atlanta.
+                            </p>
+                                <div className="youtube">
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/iSgUMPHQEWw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+
                     </div>
-                    <br/>
-                        <p className="unlike-text-2">
-                            “I help share my sound from the studio to others, and all around the world” Valentine explains. “[With 6lack] there’s no wasted time or wasted music because you’re able to communicate things right away.”
-                            Ricardo Valdez Valentine
-                            Artist, East Atlanta.
-                        </p>
+
+
                 </div>
 
-                
             </div>
+            
          );
     }
 }
