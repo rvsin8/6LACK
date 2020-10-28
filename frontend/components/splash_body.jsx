7198dad3d44a1b6533d545c65611f55559da5d14
“Ravneet <your_email_here>
2020-10-28 18:53:44 -0400

    some tasks done

--- frontend/components/splash_body.jsx
+++ frontend/components/splash_body.jsx
@@ -7,15 +7,13 @@ const NavBar = props => {
         <ul className="session-nav">  
             <img src={window.logo} className="splash-picture" />
 
-            <li><Link to="/" className="navbar-link">Why 6lack?</Link></li>
             <a className="navbar-link" href="https://github.com/rvsin8"><p>GitHub</p></a>
             <a className="navbar-link" href="https://www.linkedin.com/in/ravneet-singh-20b978a4/">LinkedIn</a>
 
             
-            <li><Link to="/login" className="navbar-link">Contact Sales</Link></li>
             <li><Link to="/login" className="navbar-link">Log In</Link></li>
             <div classname="tired">
-                <li><Link to="/signup" className="navbar-right">LAUNCH 6LACK</Link></li>
+                <li><Link to="/signup" className="navbar-right">Try For Free</Link></li>
 
             </div>
             
--- frontend/components/splash_header.jsx
+++ frontend/components/splash_header.jsx
@@ -32,7 +32,6 @@ class SplashPage extends React.Component {
     }
     
     render() {
-        console.log(this.props)
         return (
             <div>
                 <Navbar/>
