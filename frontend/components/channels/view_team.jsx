7ee1eee49bad0b9bc4a806d5a4e8d290a0aa59ac
“Ravneet <your_email_here>
2020-11-02 12:38:36 -0500

    header, team, channels

--- frontend/components/channels/view_team.jsx
+++ frontend/components/channels/view_team.jsx
@@ -3,22 +3,36 @@ import Channels from "../channels/channels";
 import Teams from "../channels/teams";
 import Header from "../channels/header";
 import Messages from "../channels/messages";
-import Input from "../channels/input";
+import SendMessage from "../channels/sendmessage";
 import AppLayout from "../channels/appLayout";
 
 export default () => (
   <AppLayout>
-    <Teams>Teams</Teams>
-    <Channels>Channels</Channels>
-    <Header>Header</Header>
+    <Teams
+      teams={[
+        { id: 1, letter: "B" },
+        { id: 2, letter: "Q" },
+      ]}
+    />
+    <Channels
+      teamName="Team name"
+      username="Username"
+      channels={[
+        { id: 1, name: "general" },
+        { id: 2, name: "random" },
+      ]}
+      users={[
+        { id: 1, name: "slackbot" },
+        { id: 2, name: "ravneetsingh" },
+      ]}
+    />
+    <Header channelName="general" />
     <Messages>
       <ul className="message-list">
         <li />
         <li />
       </ul>
     </Messages>
-    <Input>
-      <input type="text" placeholder="CSS Grid Layout Module" />
-    </Input>
+    <SendMessage channelName="general" />
   </AppLayout>
 );
