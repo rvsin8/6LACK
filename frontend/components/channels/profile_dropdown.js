7ccae2a1353f78e3c4f4030c42473e1000f53451
rvsin8 <rsingh0815@gmail.com>
2020-11-10 13:09:44 -0500

    dropdown item

--- frontend/components/channels/profile_dropdown.js
+++ frontend/components/channels/profile_dropdown.js
@@ -1,14 +1,27 @@
 function DropdownMenu() {
-  const [activeMenu, setActiveMenu] = useState('main');
+  const [activeMenu, setActiveMenu] = useState("main");
   const [menuHeight, setMenuHeight] = useState(null);
   const dropdownRef = useRef(null);
 
   useEffect(() => {
-    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
-  }, [])
-  
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
   function calcHeight(el) {
     const height = el.offsetHeight;
     setMenuHeight(height);
   }
+}
+function DropdownItem(props) {
+  return (
+    <a
+      href="#"
+      className="menu-item"
+      onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+    >
+      <span className="icon-button">{props.leftIcon}</span>
+      {props.children}
+      <span className="icon-right">{props.rightIcon}</span>
+    </a>
+  );
 }
