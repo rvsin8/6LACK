849907ad100dee61651b4daad35afb710467d8d2
rvsin8 <rsingh0815@gmail.com>
2020-11-10 13:07:31 -0500

    dropdown logic

---
+++ frontend/components/channels/profile_dropdown.js
@@ -0,0 +1,8 @@
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
