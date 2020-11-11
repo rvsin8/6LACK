72dba118522b75e7535dbe7f8eee108456e9ad84
rvsin8 <rsingh0815@gmail.com>
2020-11-11 12:17:57 -0500

    real time bugs / fix css

--- frontend/components/channels/home.jsx
+++ frontend/components/channels/home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         onChange={handleRoomNameChange}
         className="text-input-field"
       />
-      <Link to={`/${roomName}`} className="enter-room-button">
+      <Link to={`/channel/${roomName}`} className="enter-room-button">
         Join room
       </Link>
     </div>
