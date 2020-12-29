0d3cffde49b9c35252b5ae57c0a19147cee213e3
rvsin8 <rsingh0815@gmail.com>
2020-12-28 20:09:00 -0500

    channel dms, figure out how to implement toggle

---
+++ frontend/components/newchannels/channel_sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class ChannelSidebar extends React.Component{
+    constructor(props){
+        super();
+
+        this.state = {
+            title: '',
+            channel_type: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    };
+
+    componentDidMount(){
+        this.props.fetchUsers();
+        this.props.fetchChannels();
+    }
+
+    handleInput(type) {
+        return (event) => {
+            this.setState({ [type]: event.target.value });
+        };
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const channel = Object.assign({}, this.state);
+        this.props.createChannel(channel);
+    };
+    
+    
+    //https://medium.com/path2code/how-react-js-toggle-button-works-99c838ae2fe1#:~:text=Function%20to%20make%20Toggle%20Button%20works&text=It%20causes%20React%20to%20re,passing%20a%20function%20into%20this.
+    toggleChannels() {
+
+    };
+
+    toggleDms() {
+
+
+
+    render(){
+
+        
+        
+    }
+}
+
+export default ChannelSidebar;
