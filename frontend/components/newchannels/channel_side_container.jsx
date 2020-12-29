d7d5d297d61359b90dc0dc29c667e33a76029698
rvsin8 <rsingh0815@gmail.com>
2020-12-28 19:59:22 -0500

    side bar for channels

---
+++ frontend/components/newchannels/channel_side_container.jsx
@@ -0,0 +1,30 @@
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {fetchChannels,fetchChannel,createChannel,updateChannel,deleteChannel} from '../../actions/channel_actions'; //rework actions
+import {fetchUsers} from '../../actions/user_actions';
+import { openModal } from '../../actions/modal_actions'; //rework
+import ChannelSidebar from './channel_sidebar';
+
+const msp = (state, ownProps) => {
+    return {
+        currentUser: state.entities.users[state.session.user.id],
+        channels: state.entities.channels,
+        currentChannelId: ownProps.match.params.channelId,
+        users: state.entities.users
+    }
+}
+
+const mdp = dispatch => {
+    return {
+        fetchChannels: () => dispatch(fetchChannels()),
+        createChannel: channel => dispatch(createChannel(channel)),
+        fetchChannel: channel => dispatch(fetchChannel(channel)),
+        updateChannel: channel => dispatch(updateChannel(channel)),
+        deleteChannel: channel => dispatch(deleteChannel(channel)),
+        logout: () => dispatch(logout()),
+        openModal: modal => dispatch(openModal(modal)),
+        fetchUsers: () => dispatch(fetchUsers())
+    }
+}
+
+export default withRouter(connect(msp,mdp)(ChannelSidebar));
