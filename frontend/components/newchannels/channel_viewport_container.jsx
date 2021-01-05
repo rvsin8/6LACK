71ebc9e81c402a45397ca3fbd256c6f8b016b463
rvsin8 <rsingh0815@gmail.com>
2021-01-04 22:47:06 -0500

    viewpoints

---
+++ frontend/components/newchannels/channel_viewport_container.jsx
@@ -0,0 +1,23 @@
+import {connect} from 'react-redux';
+import {logout} from '../../actions/session_actions';
+import {openModal} from '../../actions/modal_actions';
+import {updateUser} from '../../actions/user_actions';
+import ChannelViewport from './channel_viewport';
+
+const msp = state => {
+    return {
+    users: state.entities.users,
+    currentUser: state.entities.users[state.session.user.id],
+    channels: state.entities.channels
+}}
+
+const mdp = dispatch => {
+    return {
+        openModal: modal => dispatch(openModal(modal)),
+        logout: () => dispatch(logout()),
+        updateUser: user => dispatch(updateUser(user))
+        
+    }
+}
+
+export default connect(msp,mdp)(ChannelViewport);
