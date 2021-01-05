02bf80c6899ee660d33f5435ef1273eedc383576
rvsin8 <rsingh0815@gmail.com>
2021-01-04 22:56:40 -0500

    added channel error reducers

---
+++ frontend/reducers/channel_error_reducers.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CHANNEL_ERRORS, CLEAR_CHANNEL_ERRORS } from '../actions/channel_actions';
+
+const channelErrorsReducer = (defaultState = [], action) => {
+
+    Object.freeze(defaultState);
+
+    switch (action.type) {
+        case RECEIVE_CHANNEL_ERRORS:
+            return action.errors;
+        case CLEAR_CHANNEL_ERRORS:
+            return [];
+        default:
+            return defaultState;
+    }
+}
+
+export default channelErrorsReducer;
