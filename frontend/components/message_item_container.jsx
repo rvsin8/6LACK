46d4e2b25009dcf2ad4e605af9bb35a3450b3158
rvsin8 <rsingh0815@gmail.com>
2021-01-06 18:32:14 -0500

    message item container

---
+++ frontend/components/message_item_container.jsx
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux';
+
+import MessageItem from './message_item';
+
+import { updateMessage, fetchMessages } from '../../actions/message_actions';
+
+const msp = state => {
+    return {
+        currentUserId: state.entities.users[state.session.user.id].id
+    }
+}
+
+const mdp = dispatch => {
+    return {
+        updateMessage: message => dispatch(updateMessage(message)),
+        fetchMessages: channelId => dispatch(fetchMessages(channelId))
+    }
+}
+
+export default connect(msp,mdp)(MessageItem);
