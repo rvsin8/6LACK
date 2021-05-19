216da76f67d92212b32712b5daf1b467ad2f1f50
rvsin8 <rsingh0815@gmail.com>
2021-05-19 15:03:50 -0400

    update

---
+++ frontend/components/header._container.jsx
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions";
+import Header from "./header";
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.entities.users[state.session.id],
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(logout()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ frontend/components/header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    debugger;
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <NavLink to={"/"} className="logo">
+          <h2>TENSE</h2>
+        </NavLink>
+        <button onClick={this.handleLogout}>Log Out</button>
+      </div>
+    );
+  }
+}
+
+export default Header;
