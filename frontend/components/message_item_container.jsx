508cb0590d14906df3e1e11404083734176eac31
rvsin8 <rsingh0815@gmail.com>
2021-05-18 23:42:59 -0400

    redo message item container

--- frontend/components/message_item_container.jsx
+++ frontend/components/message_item_container.jsx
@@ -1,20 +1,18 @@
-// import {connect} from 'react-redux';
+import {connect} from 'react-redux';
+import MessageItem from './message_item';
+import { updateMessage, fetchMessages } from '../actions/message_actions';
 
-// import MessageItem from './message_item';
+const msp = state => {
+    return {
+       currentUserId: state.entities.users[state.session.user.id].id
+    }
+}
 
-// import { updateMessage, fetchMessages } from '../actions/message_actions';
+const mdp = dispatch => {
+    return {
+        updateMessage: message => dispatch(updateMessage(message)),
+        fetchMessages: channelId => dispatch(fetchMessages(channelId))
+    }
+}
 
-// const msp = state => {
-//     return {
-//        currentUserId: state.entities.users[state.session.user.id].id
-//     }
-// }
-
-// const mdp = dispatch => {
-//     return {
-//         updateMessage: message => dispatch(updateMessage(message)),
-//         fetchMessages: channelId => dispatch(fetchMessages(channelId))
-//     }
-// }
-
-// export default connect(msp,mdp)(MessageItem);
+export default connect(msp,mdp)(MessageItem);
