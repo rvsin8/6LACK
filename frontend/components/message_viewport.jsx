18f620a7a9b710ef3878eb6b493bbd5f0d7b0acb
rvsin8 <rsingh0815@gmail.com>
2021-05-18 23:44:49 -0400

    redo message viewport

--- frontend/components/message_viewport.jsx
+++ frontend/components/message_viewport.jsx
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import MessageItemContainer from './message_item_container';
+import React from 'react';
+import MessageItemContainer from './message_item_container';
 
-// export default class MessagesViewport extends React.Component{
-//     constructor(props){
-//         super(props)
-//     }
+export default class MessagesViewport extends React.Component{
+    constructor(props){
+        super(props)
+    }
 
-//     componentDidMount(){
-//         this.props.fetchMessages(this.props.currentChannelId);
-//     }
+    componentDidMount(){
+        this.props.fetchMessages(this.props.currentChannelId);
+    }
 
-//     componentDidUpdate(){
-//         let viewport;
-//         if (this.props.messages) {
-//           viewport = document.getElementById("messages-ul");
-//           viewport.scrollTop = viewport.scrollHeight;
-//         }
-//     }
+    componentDidUpdate(){
+        let viewport;
+        if (this.props.messages) {
+          viewport = document.getElementById("messages-ul");
+          viewport.scrollTop = viewport.scrollHeight;
+        }
+    }
 
-//     render(){
+    render(){
 
-//         this.messagesArray = Object.values(this.props.messages)
+        this.messagesArray = Object.values(this.props.messages)
 
-//         this.currentChannelMessages = this.messagesArray.filter(message => message.channel_id == this.props.currentChannelId)
+        this.currentChannelMessages = this.messagesArray.filter(message => message.channel_id == this.props.currentChannelId)
 
-//         if (this.props.messages) {
-//             return (
-//                 <>
-//                 <ul id="messages-ul" className='messages-ul'>
+        if (this.props.messages) {
+            return (
+                <>
+                <ul id="messages-ul" className='messages-ul'>
 
-//                     {this.currentChannelMessages.map(message => {
-//                         return(
-//                             <MessageItemContainer key={message.id} message={message}/>
+                    {this.currentChannelMessages.map(message => {
+                        return(
+                            <MessageItemContainer key={message.id} message={message}/>
                             
-//                         )
-//                     })}
-//                 </ul>
-//                 </>
-//             )
-//         } else {
-//             return null;
-//         }
-//     }
-// }
+                        )
+                    })}
+                </ul>
+                </>
+            )
+        } else {
+            return null;
+        }
+    }
+}
