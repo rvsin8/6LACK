a6b0883e86bc9c3311699bb93b2b22e2a4e75e46
rvsin8 <rsingh0815@gmail.com>
2021-12-13 09:47:42 -0500

    update

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -16,7 +16,7 @@ class Api::SessionsController < ApplicationController
     end
   end
   #commit purposes
-  #I would like to add daterange control and when click image, want to show image zoom viewer.
+
   #I would like to add daterange control and when click image, want to show image zoom viewer.
 
 
