2ae15bbc9a7d4b32c661fd457727972357c097e3
rvsin8 <rsingh0815@gmail.com>
2021-12-15 07:53:04 -0500

    update

--- frontend/components/searchbar_container.jsx
+++ frontend/components/searchbar_container.jsx
@@ -5,23 +5,25 @@ import { openModal, closeModal } from '../actions/modal_actions'; //rework this
 //https://www.freecodecamp.org/news/how-to-simplify-state-in-your-react-app-redux-with-a-twist-41b0e5b12dcb/#:~:text=Store%3A%20This%20is%20the%20single,change%20in%20the%20app%20state.
 const msp = state => {
     return {
-        users: state.entities.users, 
+        users: state.entities.users,
         currentUser: state.session.user,
         channels: state.entities.channels,
         messages: state.entities.messages,
-    //    currentUserEmail: state.entities.users[state.session.user.id].email
+        //    currentUserEmail: state.entities.users[state.session.user.id].email
     };
 };
 
+//commit purposes
+
 const mdp = dispatch => {
-    
-    return{
+
+    return {
         openModal: () => dispatch(openModal()),
         closeModal: () => dispatch(closeModal())
     };
 
 };
 
-export default connect(msp,mdp)(Searchbar);
+export default connect(msp, mdp)(Searchbar);
 
 //no openmodal, closedmodal does not even show up
