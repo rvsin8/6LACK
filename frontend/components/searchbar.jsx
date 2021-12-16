bd29155fb57ed8cf85c8992e669801e4896eb924
rvsin8 <rsingh0815@gmail.com>
2021-12-16 08:14:22 -0500

    update

--- frontend/components/searchbar.jsx
+++ frontend/components/searchbar.jsx
@@ -41,7 +41,7 @@ export default class Searchbar extends React.Component {
 
   componentDidUpdate(prevProps) {
     //console.log('current props', this.props);
-    console.log("prev props", prevProps);
+    //console.log("prev props", prevProps);
     if (JSON.stringify(prevProps.channels) !== JSON.stringify(this.props.channels)) {
       console.log("UPDATING CHANNEL LIST NOWWW");
       this.setState({
