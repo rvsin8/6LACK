bb22349abbd1213faa3e7efbc69080e7c8fc2a09
rvsin8 <rsingh0815@gmail.com>
2021-12-20 09:54:41 -0500

    update

--- frontend/components/searchbar.jsx
+++ frontend/components/searchbar.jsx
@@ -39,8 +39,6 @@ export default class Searchbar extends React.Component {
     this.setState({ filteredChannelsArray: Object.values(this.props.channels) });
   }
 
-  //commit man commit
-
   componentDidUpdate(prevProps) {
     //console.log('current props', this.props);
     //console.log("prev props", prevProps);
