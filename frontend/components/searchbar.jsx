7207635e3c2fc92d18c16877c86d67de2b8c2a08
rvsin8 <rsingh0815@gmail.com>
2022-01-11 20:04:45 -0500

    update

--- frontend/components/searchbar.jsx
+++ frontend/components/searchbar.jsx
@@ -119,8 +119,6 @@ export default class Searchbar extends React.Component {
       </>
     )
 
-    //oz
-
     const noSearchResults = <div id="search-filler">Narrow your search</div>
 
     //console.log('SearchValue', this.state.searchValue);
