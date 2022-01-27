fdf2822029e60fcf2ea6229949d85128326cbfee
rvsin8 <rsingh0815@gmail.com>
2022-01-27 18:06:41 -0500

    first commit

--- frontend/components/searchbar.jsx
+++ frontend/components/searchbar.jsx
@@ -119,8 +119,6 @@ export default class Searchbar extends React.Component {
       </>
     )
 
-    //we bac
-
     const noSearchResults = <div id="search-filler">Narrow your search</div>
 
     //console.log('SearchValue', this.state.searchValue);
