606fb8d2a9fa6e2f8af79be117d2d7f7b7368712
rvsin8 <rsingh0815@gmail.com>
2022-02-10 08:56:24 -0500

    first commit

--- frontend/components/searchbar.jsx
+++ frontend/components/searchbar.jsx
@@ -71,8 +71,6 @@ export default class Searchbar extends React.Component {
     }
   }
 
-  //crossfit gang dont bang
-
   displayTitle(title) {
     const channelDisplayTitleArray = title.split(', ')
     const currentUserRemoved = channelDisplayTitleArray.filter(
