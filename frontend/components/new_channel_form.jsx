b412ef4da7922a75587c4fdee30fbb386f55dba9
rvsin8 <rsingh0815@gmail.com>
2022-03-23 21:21:42 -0400

    first commit

--- frontend/components/new_channel_form.jsx
+++ frontend/components/new_channel_form.jsx
@@ -1,64 +1,62 @@
-import React from "react";
+import React from 'react'
 
 class NewChannelForm extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      title: "",
-      description: "",
-      channel_type: "public",
+      title: '',
+      description: '',
+      channel_type: 'public',
       users: [],
-      channel_or_dm: "channel",
-    };
+      channel_or_dm: 'channel',
+    }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
 
-    this.togglePrivate = this.togglePrivate.bind(this);
+    this.togglePrivate = this.togglePrivate.bind(this)
   }
 
   componentDidMount() {
-    this.props.clearErrors();
+    //
+    this.props.clearErrors()
   }
 
   handleInput(type) {
     return (event) => {
-      this.setState({ [type]: event.target.value });
-    };
+      this.setState({ [type]: event.target.value })
+    }
   }
 
-
   handleSubmit(event) {
-    event.preventDefault();
-    const channel = Object.assign({}, this.state);
-    this.props.createChannel(channel).then(
-      (res) => {
-        this.props.history.push(`/channels/${res.channel.id}`);
-        this.props.closeModal();
-      }
-    );
+    event.preventDefault()
+    const channel = Object.assign({}, this.state)
+    this.props.createChannel(channel).then((res) => {
+      this.props.history.push(`/channels/${res.channel.id}`)
+      this.props.closeModal()
+    })
   }
 
   togglePrivate() {
-    if (this.state.channel_type === "public") {
-      this.setState({ channel_type: "private" });
-      document.getElementById("new-channel-form-h1").innerHTML =
-        "Create a private channel";
+    if (this.state.channel_type === 'public') {
+      this.setState({ channel_type: 'private' })
+      document.getElementById('new-channel-form-h1').innerHTML =
+        'Create a private channel'
     } else {
-      this.setState({ channel_type: "public" });
-      document.getElementById("new-channel-form-h1").innerHTML =
-        "Create a channel";
+      this.setState({ channel_type: 'public' })
+      document.getElementById('new-channel-form-h1').innerHTML =
+        'Create a channel'
     }
   }
 
   componentDidUpdate() {
-    const createButton = document.getElementById("new-channel-submit-button");
+    const createButton = document.getElementById('new-channel-submit-button')
     if (this.state.title) {
-      createButton.classList.add("enabled-button");
-      createButton.classList.remove("disabled-button");
+      createButton.classList.add('enabled-button')
+      createButton.classList.remove('disabled-button')
     } else {
-      createButton.classList.add("disabled-button");
-      createButton.classList.remove("enabled-button");
+      createButton.classList.add('disabled-button')
+      createButton.classList.remove('enabled-button')
     }
   }
 
@@ -80,7 +78,6 @@ class NewChannelForm extends React.Component {
 
           <div>
             Name
-  
             <br />
             <input
               autoFocus="on"
@@ -88,14 +85,14 @@ class NewChannelForm extends React.Component {
               id="new-channel-title"
               className="new-channel-input"
               type="text"
-              onChange={this.handleInput("title")}
+              onChange={this.handleInput('title')}
               onFocus={() => {
-                document.getElementById("new-channel-title").style =
-                  "box-shadow: 0 0 0 4px #bee2f1;";
+                document.getElementById('new-channel-title').style =
+                  'box-shadow: 0 0 0 4px #bee2f1;'
               }}
               onBlur={() => {
-                document.getElementById("new-channel-title").style =
-                  "box-shadow: none;";
+                document.getElementById('new-channel-title').style =
+                  'box-shadow: none;'
               }}
               placeholder="# e.g. plan-album-release"
             />
@@ -109,14 +106,14 @@ class NewChannelForm extends React.Component {
               id="new-channel-desc"
               className="new-channel-input"
               type="text"
-              onChange={this.handleInput("description")}
+              onChange={this.handleInput('description')}
               onFocus={() => {
-                document.getElementById("new-channel-desc").style =
-                  "box-shadow: 0 0 0 4px #bee2f1;";
+                document.getElementById('new-channel-desc').style =
+                  'box-shadow: 0 0 0 4px #bee2f1;'
               }}
               onBlur={() => {
-                document.getElementById("new-channel-desc").style =
-                  "box-shadow: none;";
+                document.getElementById('new-channel-desc').style =
+                  'box-shadow: none;'
               }}
             />
             <span id="description-span">What's this channel about?</span>
@@ -156,8 +153,8 @@ class NewChannelForm extends React.Component {
         </button>
         {/* {this.renderErrors()} */}
       </div>
-    );
+    )
   }
 }
 
-export default NewChannelForm;
+export default NewChannelForm
